64acc8e0546a4e8a72b606aa72631152a2d512c8
nelsonmartino <nelsonmartino@gmail.com>
2024-10-04 12:18:43 -0300

    New images, accordeon and structure

--- client/tailwindcss-config.js
+++ client/tailwindcss-config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+import withMT from '@material-tailwind/react/utils/withMT'
+
+export default withMT({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('../client/src/assets/Background_header.png')",        
+        'hero-pattern': "url('../client/src/assets/Background_header.png')",
       },
       backgroundColor: {
         'my-custom-color': '#FFBA00',
       },
       fontFamily: {
-        serif: ["gibson", "serif"],
-        sans: ["proxima-nova", "sans-serif"],
+        gibson: ['gibson', 'serif'],
+        nova: ['proxima-nova', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
+})
